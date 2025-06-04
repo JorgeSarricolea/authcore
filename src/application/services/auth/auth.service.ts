3834c43dbfdac9c343c52cbcd4a02f74196f2b48
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2025-06-03 20:20:14 -0600

    feat(auth): create AuthService for token generation and login use case
        - Added 'AuthService' to encapsulate authentication logic.
        - Instantiates 'LoginUseCase' with necessary dependencies.
        - Implements 'generateTokens' to return access and refresh tokens.

---
+++ src/application/services/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { User } from "@/domain/entities/user.entity";
+import { IUserRepository } from "@/domain/repositories/user.repository";
+import JwtService from "@/infrastructure/external-services/auth/jwt.service";
+import {
+  LoginUseCase,
+  AuthTokens,
+} from "@/application/use-cases/user/login.use-case";
+import UserRepository from "@/infrastructure/orm/repositories/user.prisma.repository";
+import PasswordService from "@/infrastructure/external-services/encryption/password.service";
+
+export default class AuthService {
+  private loginUseCase: LoginUseCase;
+
+  constructor() {
+    const userRepository = new UserRepository();
+    const jwtService = new JwtService();
+    const passwordService = new PasswordService();
+    this.loginUseCase = new LoginUseCase(
+      userRepository,
+      passwordService,
+      jwtService
+    );
+  }
+
+  async generateTokens(user: User): Promise<AuthTokens> {
+    const tokenPayload = {
+      id: user.id,
+      email: user.email,
+      roleId: user.roles?.[0]?.id,
+      emailVerified: user.email_verified,
+    };
+
+    const jwtService = new JwtService();
+    const accessToken = jwtService.generateAccessToken(tokenPayload);
+    const refreshToken = jwtService.generateRefreshToken(tokenPayload);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
